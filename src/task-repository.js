ccb946cc22ef022299b6e4c7b206a472b4f09f3c
Christoph Hoffmann <christoph.hoffmann@nestedloop.de>
2016-08-14 23:16:05 +0200

    extend task-repository with basic crud functions

--- src/task-repository.js
+++ src/task-repository.js
@@ -1,6 +1,7 @@
 "use strict";
 const Promise = require('bluebird');
 const {InvalidArgumentError} = require('./error/invalid-argument-error');
+const forOwn = require('lodash.forown');
 
 class TaskRepository {
   constructor(collection) {
@@ -8,27 +9,87 @@ class TaskRepository {
   }
 
   create(opts) {
+    let task = Object.assign({}, opts);
     return new Promise((resolve, reject) => {
       if (!this.collection) return reject(new InvalidArgumentError('collection must exist', 'collection'));
-      if (!opts.title) return reject(new InvalidArgumentError('opts must contain title field', 'title'));
+      if (!task.title) return reject(new InvalidArgumentError('task must contain title field', 'title'));
 
-      this.collection.insert(opts, (err, task)=> {
+      if (!task.state) task.state = 'open';
+      if (!task.archived) task.archived = false;
+      task.created = task.lastModified = new Date();
+
+      this.collection.insert(task, (err, task)=> {
         if (err) return reject(err);
         resolve(task[0]);
       })
     });
   };
 
-  findAll() {
+  updateById(id, opts) {
     return new Promise((resolve, reject) => {
       if (!this.collection) return reject(new InvalidArgumentError('collection must exist', 'collection'));
 
-      this.collection.find({}).toArray((err, tasks) => {
+      let extendedOpts = Object.assign({}, opts);
+      extendedOpts.lastModified = new Date();
+
+      const changes = buildUpdateOperations(extendedOpts);
+
+      this.collection.update({_id: id}, changes, (err) => {
+        if (err) return reject(err);
+        resolve(this.findById(id))
+      })
+    });
+  }
+
+  findById(id) {
+    return new Promise((resolve, reject) => {
+      if (!this.collection) return reject(new InvalidArgumentError('collection must exist', 'collection'));
+
+      this.collection.findOne({_id: id}, (err, tasks) => {
+        if (err) return reject(err);
+        resolve(tasks);
+      });
+    });
+  }
+
+  findAll(inclArchived = false) {
+    return new Promise((resolve, reject) => {
+      if (!this.collection) return reject(new InvalidArgumentError('collection must exist', 'collection'));
+
+      var query = inclArchived ? {} : {archived: false};
+      this.collection.find(query).toArray((err, tasks) => {
         if (err) return reject(err);
         resolve(tasks);
       })
     });
   }
+
+  deleteById(id){
+    return new Promise((resolve, reject) => {
+      if (!this.collection) return reject(new InvalidArgumentError('collection must exist', 'collection'));
+
+      this.collection.remove({_id: id}, (err) => {
+        if (err) return reject(err);
+        resolve();
+      })
+    });
+  }
+}
+
+function buildUpdateOperations(fields) {
+  const changes = {
+    $set: {},
+    $unset: {}
+  };
+  forOwn(fields, (value, key) => {
+    if (value == undefined) {
+      changes.$unset[key] = true;
+    } else {
+      changes.$set[key] = value;
+    }
+  });
+
+  return changes;
 }
 
 module.exports.TaskRepository = TaskRepository;
--- test/task-repostitory.spec.js
+++ test/task-repostitory.spec.js
@@ -35,6 +35,75 @@ describe('The task repository', function() {
       }).catch(done)
   });
 
+  it('should create tasks in state "open"', function(done) {
+    this.taskRepository.create(getTaskOpts())
+      .then(task => {
+        expect(task).to.have.property('state', 'open');
+        done()
+      })
+      .catch(done);
+  });
+
+  it('should create tasks with a visibility of visible', function(done) {
+    this.taskRepository.create(getTaskOpts())
+      .then(task => {
+        expect(task).to.have.property('archived', false);
+        done()
+      })
+      .catch(done);
+  });
+
+  it('should update a task', function(done) {
+    const newTitle = 'newTitle';
+    this.taskRepository.create(getTaskOpts())
+      .then(task => {
+        expect(task).to.have.property('title', getTaskOpts().title);
+        task.title = newTitle;
+        return this.taskRepository.updateById(task._id, task)
+      })
+      .then(task => {
+        expect(task).to.have.property('title', newTitle);
+        done()
+      })
+      .catch(done);
+  });
+
+  it('update should only change fields in opts', function(done) {
+    this.taskRepository.create(getTaskOpts())
+      .then(task => {
+        return this.taskRepository.updateById(task._id, {archived: true})
+      })
+      .then(task => {
+        expect(task).to.have.property('title', getTaskOpts().title);
+        expect(task).to.have.property('archived', true);
+        done()
+      })
+      .catch(done)
+  });
+
+  it('update should remove fields if set to undefined', function(done) {
+    var extendedValues = {extended: 'awesome!'};
+    this.taskRepository.create(Object.assign(extendedValues, getTaskOpts()))
+      .then(task => {
+        expect(task).to.have.property('extended', extendedValues.extended);
+        return this.taskRepository.updateById(task._id, {extended: undefined})
+      })
+      .then(task => {
+        expect(task).to.not.have.property('extended');
+        done()
+      })
+      .catch(done);
+  });
+  
+  it('should find an task by id', function(done) {
+    this.taskRepository.create(getTaskOpts())
+      .then(task => this.taskRepository.findById(task._id))
+      .then(task => {
+        expect(task).to.have.property('title', getTaskOpts().title);
+        done()
+      })
+  });
+
   it('should list all tasks', function(done) {
     this.taskRepository.create(getTaskOpts())
       .then(() => this.taskRepository.findAll())
@@ -52,9 +121,26 @@ describe('The task repository', function() {
         done()
       })
       .catch(done)
+  });
+  
+  it('should delete a task with a given id', function(done) {
+    var id;
+    this.taskRepository.create(getTaskOpts())
+      .then(t => id = t._id)
+      .then(() => this.taskRepository.findById(id))
+      .then(t => {
+        expect(t).to.have.property('title', getTaskOpts().title);
+        return this.taskRepository.deleteById(t._id);
+      })
+      .then(() => this.taskRepository.findById(id))
+      .then(t => {
+        expect(t).to.be.null;
+        done();
+      })
+      .catch(done)
   })
 });
 
-function getTaskOpts(){
+function getTaskOpts() {
   return {title: 'test'}
 }
